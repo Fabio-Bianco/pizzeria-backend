292403978433c6f386e676b61a41570874d74799
Fabio-Bianco <biancobfabio@gmail.com>
2025-11-17 15:41:18 +0100

    Aggiungi 'Anidride Solforosa' alla lista degli allergeni nel seeder

--- database/seeders/AllergenSeeder.php
+++ database/seeders/AllergenSeeder.php
@@ -24,6 +24,7 @@ class AllergenSeeder extends Seeder
             'Sedano',
             'Senape',
             'Solfiti',
+            'Anidride Solforosa',
             'Lupini',
             'Nichel',
         ];
